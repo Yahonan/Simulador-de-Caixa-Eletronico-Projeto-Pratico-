7960b08dc5195972c1c263e9c89a205aa702cb78
Yahonan <110312787+Yahonan@users.noreply.github.com>
2025-05-17 13:34:54 +0000

    Adicionado função de ver saldo e adicionar saldo

--- simulador.py
+++ simulador.py
@@ -4,3 +4,19 @@ from tkinter import messagebox
 saldo = 1000.0
 usuario_correto = "admin"
 senha_correta = "1234"
+
+def ver_saldo():
+    messagebox.showinfo("Saldo", f"Seu saldo atual é: R$ {saldo:.2f}")
+
+def depositar():
+    global saldo
+    try:
+        valor = float(entrada_valor.get())
+        if valor <= 0:
+            messagebox.showerror("Erro", "Digite um valor maior que zero.")
+        else:
+            saldo += valor
+            messagebox.showinfo("Depósito", f"Depósito de R$ {valor:.2f} realizado com sucesso.")
+            entrada_valor.delete(0, tk.END)
+    except ValueError:
+        messagebox.showerror("Erro", "Digite um valor válido.")
